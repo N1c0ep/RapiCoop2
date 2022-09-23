761e9cd621ceaadf9f36d4853172ebe685f967fb
Dad0723 <daniel.latino2009@gmail.com>
2022-09-22 20:50:03 -0500

    i

--- app/src/main/java/co/edu/unipiloto/rapicoop2/Login_form.java
+++ app/src/main/java/co/edu/unipiloto/rapicoop2/Login_form.java
@@ -3,21 +3,42 @@ package co.edu.unipiloto.rapicoop2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login_form extends AppCompatActivity {
+    Button usuario;
     @Override
     protected void onCreate (Bundle savedInstanceState){
+
+        //usuario = (Button) findViewById(R.id.usuario);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
         getSupportActionBar().setTitle("Login Form");
+
+        /*usuario.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Login_form.this,Usuario.class);
+                startActivity(i);
+            }
+        });*/
     }
+
     public void btn_singupForm(View view){
         startActivity(new Intent(getApplicationContext(),Singup_Form.class));
     }
 
-    public void btn_ingresar(View view) {
-        startActivity(new Intent(getApplicationContext(),Usuario.class));
+    public void btn_ingresar(View view){
+        Intent i = new Intent(Login_form.this,Usuario.class);
+        startActivity(i);
     }
+
+
+
 }
+
+
--- app/src/main/java/co/edu/unipiloto/rapicoop2/Usuario.java
+++ app/src/main/java/co/edu/unipiloto/rapicoop2/Usuario.java
@@ -1,6 +1,9 @@
 package co.edu.unipiloto.rapicoop2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,6 @@ public class Usuario extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario);
+
     }
 }
